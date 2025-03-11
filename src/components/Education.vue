2f1cf4f5b2f577593892b3c6d47a248fbe040f10
stellamarma <stellamarmat@gmail.com>
2025-03-11 12:43:57 +0200

    Add css on educational

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -4,9 +4,10 @@
   
       <div class="row mt-4">
         <div class="col-12">
-          <div class="h-100 p-5 bg-body-tertiary border rounded-3">
-            <h2>Add borders</h2>
-            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+          <div class="h-100 p-5 custom-bg-red border rounded-3">
+            <h2>Bachelor Of Electrical and Computer Engineering</h2>
+            <p>University of Western Macedonia</p>
+            <a href="https://dspace.uowm.gr/xmlui/handle/123456789/4759"><button type="button" class="btn btn-outline-dark">Thesis</button></a>
           </div>
         </div>
       </div>
@@ -14,18 +15,35 @@
       <!-- Νέα διάταξη με δύο στήλες κάτω -->
       <div class="row mt-4">
         <div class="col-6">
-          <div class="h-100 p-5 text-bg-dark rounded-3">
-            <h2>Change the background</h2>
-            <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+          <div class="h-100 p-5 custom-bg-blue border rounded-3">
+            <h2>Bootcamp:</h2>
+            <p>Full-Stack Web Developer</p>
+            <a href="https://github.com/stellamarma/Web_Develop_projects"><button type="button" class="btn btn-outline-dark">Portfolio</button></a>
           </div>
         </div>
         <div class="col-6">
-          <div class="h-100 p-5 bg-body-tertiary border rounded-3">
-            <h2>Add borders</h2>
-            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+          <div class="h-100 p-5 custom-bg-green border rounded-3">
+            <h2>Cloud platforms</h2>
+            <p> 
+              AWS Academy Graduate – AWS Academy Cloud Foundations
+            </p>
           </div>
         </div>
       </div>
     </div>
   </template>
+  
+  <style scoped>
+    .custom-bg-red {
+      background-color: rgb(116, 185, 255); /* Κόκκινο */
+    }
+    
+    .custom-bg-blue {
+      background-color: rgb(162, 155, 254); /* Μπλε */
+    }
+  
+    .custom-bg-green {
+      background-color: rgb(223, 230, 233); /* Πράσινο */
+    }
+  </style>
   
