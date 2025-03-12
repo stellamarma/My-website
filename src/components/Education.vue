831725885c0a9df63d79662edb84f1c9b0684ff7
stellamarma <stellamarmat@gmail.com>
2025-03-12 18:46:20 +0200

    Add css for education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -4,33 +4,38 @@
   
       <div class="row mt-4">
         <div class="col-12">
-          <div class="h-100 p-5 custom-bg-red border rounded-3">
+          <div class="h-100 p-5 custom-bg-red border rounded-3 text-white">
             <h2>Bachelor Of Electrical and Computer Engineering</h2>
             <p>University of Western Macedonia</p>
-            <a href="https://dspace.uowm.gr/xmlui/handle/123456789/4759"><button type="button" class="btn btn-outline-dark">Thesis</button></a>
+            <a href="https://dspace.uowm.gr/xmlui/handle/123456789/4759"><button type="button" class="btn btn-outline-light mt-3">Thesis</button></a>
           </div>
         </div>
       </div>
-      
+    
       <!-- Νέα διάταξη με δύο στήλες κάτω -->
-      <div class="row mt-4">
-        <div class="col-6">
-          <div class="h-100 p-5 custom-bg-blue border rounded-3">
-            <h2>Bootcamp:</h2>
-            <p>Full-Stack Web Developer</p>
-            <a href="https://github.com/stellamarma/Web_Develop_projects"><button type="button" class="btn btn-outline-dark">Portfolio</button></a>
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="h-100 p-5 custom-bg-green border rounded-3">
-            <h2>Cloud platforms</h2>
-            <p> 
-              AWS Academy Graduate – AWS Academy Cloud Foundations
-            </p>
-          </div>
-        </div>
+  <div class="row py-4">
+    <div class="col-md-6 mb-4">
+      <div class="h-100 p-4 custom-bg-blue border rounded-3 shadow-sm">
+        <h2 class="mb-3 text-white">Bootcamp:</h2>
+        <p class="text-white">Full-Stack Web Developer</p>
+        <a href="https://github.com/stellamarma/Web_Develop_projects">
+          <button type="button" class="btn btn-outline-light mt-3">Portfolio</button>
+        </a>
       </div>
     </div>
+
+    <div class="col-md-6 mb-4">
+      <div class="h-100 p-4 custom-bg-green border rounded-3 shadow-sm">
+        <h2 class="mb-3 text-white">Cloud Platforms:</h2>
+        <p class="text-white">
+          AWS Academy Graduate – AWS Academy Cloud Foundations
+        </p>
+      </div>
+    </div>
+  </div>
+
+</div>
+   
   </template>
   
   <style scoped>
@@ -43,7 +48,7 @@
     }
   
     .custom-bg-green {
-      background-color: rgb(223, 230, 233); /* Πράσινο */
+      background-color: rgba(70, 158, 192, 0.759); /* Πράσινο */
     }
   </style>
   
